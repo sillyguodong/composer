c168685121c3d98692412d4fce1df800b8bb640f
sillyguodong <gedong_1994@163.com>
2023-06-14 18:24:02 +0800

    test

--- composer-action.bash
+++ composer-action.bash
@@ -46,7 +46,7 @@ chmod +x "${github_action_path}/composer.phar"
 # adapt Gitea Actions container mode
 action_path="$ACTION_PATH"
 # command_string=""
-command_string="bash -c mkdir /usr/local/bin/composer && cp ${action_path:4}/composer.phar /usr/local/bin/composer && "
+command_string="cp ${action_path:4}/composer.phar /usr/local/bin/composer ; "
 
 # command_string is passed directly to the docker executable. It includes the
 # container name and version, and this script will build up the rest of the
@@ -69,7 +69,7 @@ then
 	then
 		if [ -n "$ACTION_SSH_PORT" ]
 		then
-			ssh-keyscan -t rsa -p $ACTION_SSH_PORT "$ACTION_SSH_DOMAIN" >> ~/.ssh/known_hosts
+			ssh-keyscan -t rsa -p $ACTION_SSH_PORT "$ACTION_SSH_DOMAI" >> ~/.ssh/known_hosts
                 else
 			ssh-keyscan -t rsa "$ACTION_SSH_DOMAIN" >> ~/.ssh/known_hosts
 		fi
