2cf71e9f491d71a55f519290351022638d8c97b4
sillyguodong <gedong_1994@163.com>
2023-06-14 18:11:09 +0800

    sudo

--- composer-action.bash
+++ composer-action.bash
@@ -46,7 +46,7 @@ chmod +x "${github_action_path}/composer.phar"
 # adapt Gitea Actions container mode
 action_path="$ACTION_PATH"
 # command_string=""
-command_string="mkdir -p /usr/local/bin/composer && cp ${action_path:4}/composer.phar /usr/local/bin/composer && "
+command_string="bash -c sudo mkdir /usr/local/bin/composer && cp ${action_path:4}/composer.phar /usr/local/bin/composer && "
 
 # command_string is passed directly to the docker executable. It includes the
 # container name and version, and this script will build up the rest of the
