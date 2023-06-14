aa9818cd05cbb71607ff0087651b7371be192d36
sillyguodong <gedong_1994@163.com>
2023-06-14 17:58:00 +0800

    test cat

--- composer-action.bash
+++ composer-action.bash
@@ -46,8 +46,9 @@ chmod +x "${github_action_path}/composer.phar"
 # adapt Gitea Actions container mode
 mkdir -p /usr/local/bin/composer
 action_path="$ACTION_PATH"
-command_string=""
-# command_string="bash -c mv ${action_path:4}/composer.phar /usr/local/bin/composer && "
+# command_string=""
+cat ${action_path:4}/composer.phar
+command_string="bash -c mv ${action_path:4}/composer.phar /usr/local/bin/composer && "
 
 # command_string is passed directly to the docker executable. It includes the
 # container name and version, and this script will build up the rest of the
